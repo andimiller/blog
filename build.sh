a19db1a734e87f79cbbccce4f3e763bb9d6f576b
Andi Miller <andi@andimiller.net>
2021-09-04 01:40:57 +0100

    add html generation

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 echo "building pdf"
 pandoc -f markdown+pipe_tables+yaml_metadata_block cv.md --lua-filter=newpage.lua -H header.tex --template template.tex -t pdf > cv.pdf
 echo "building plain markdown"
-pandoc -f markdown+pipe_tables+yaml_metadata_block cv.md --lua-filter=newpage.lua -t markdown > plaincv.md
+pandoc -f markdown+pipe_tables+yaml_metadata_block cv.md --lua-filter=newpage.lua -t markdown_mmd > plaincv.md
+echo "building html"
+pandoc -f markdown+pipe_tables+yaml_metadata_block cv.md --lua-filter=newpage.lua -t html -s --css my.css > cv.html
