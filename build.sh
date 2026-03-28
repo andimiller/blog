2f751015a836b4d0726f8f086cce5239e97f932a
Andi Miller <andi@andimiller.net>
2026-03-28 22:04:04 +0000

    make qr code crisp

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo "building pdf"
 pandoc -f markdown+pipe_tables+yaml_metadata_block cv.md --lua-filter=newpage.lua -H header.tex --template template.tex -t pdf --filter=./dates.py > cv.pdf
 echo "generating QR code"
 qrencode -t SVG -o images/qr-card.svg --foreground=86B3EB --background=1d1f21 -l M "https://andimiller.net/card"
+sed -i 's/<svg /<svg shape-rendering="crispEdges" /' images/qr-card.svg
 echo "updating cabal"
 cabal update
 echo "building site"
